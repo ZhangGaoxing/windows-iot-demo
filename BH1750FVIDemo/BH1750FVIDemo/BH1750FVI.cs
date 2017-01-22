0e445e5f5319b79e85a65c54a676c98759f1a2be
ZhangGaoxing <993638176@qq.com>
2017-01-22 15:58:34 +0800

    Light Sensor
    BH1750FVI Demo

---
+++ BH1750FVIDemo/BH1750FVIDemo/BH1750FVI.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Devices.I2c;
+
+namespace BH1750FVIDemo
+{
+    /// <summary>
+    /// I2C Address Setting
+    /// </summary>
+    enum AddressSetting
+    {
+        /// <summary>
+        /// ADD Pin connect to high power level
+        /// </summary>
+        AddPinHigh = 0x5C,
+        /// <summary>
+        /// ADD Pin connect to low power level 
+        /// </summary>     
+        AddPinLow = 0x23            
+    };
+
+    /// <summary>
+    /// The mode of measuring
+    /// </summary>
+    enum MeasurementMode
+    {
+        /// <summary>
+        /// Start measurement at 1 lx resolution
+        /// </summary>
+        ContinuouslyHighResolutionMode = 0x10,
+        /// <summary>
+        /// Start measurement at 0.5 lx resolution
+        /// </summary>
+        ContinuouslyHighResolutionMode2 = 0x11,
+        /// <summary>
+        /// Start measurement at 4 lx resolution
+        /// </summary>
+        ContinuouslyLowResolutionMode = 0x13,
+        /// <summary>
+        /// Start measurement at 1 lx resolution once
+        /// </summary>
+        OneTimeHighResolutionMode = 0x20,
+        /// <summary>
+        /// Start measurement at 0.5 lx resolution once
+        /// </summary>
+        OneTimeHighResolutionMode2 = 0x21,
+        /// <summary>
+        /// Start measurement at 4 lx resolution once
+        /// </summary>
+        OneTimeLowResolutionMode = 0x23
+    }
+
+    /// <summary>
+    /// Setting light transmittance
+    /// </summary>
+    enum LightTransmittance
+    {
+        Fifty,
+        Eighty,
+        Hundred,
+        Hundred_Twenty,
+        Hundred_Fifty,
+        Two_Hundred
+    }
+
+    class BH1750FVI
+    {
+        I2cDevice sensor;
+        private byte sensorAddress;                             
+        private byte sensorMode;
+        private byte sensorResolution = 1;
+        private double sensorTransmittance = 1;
+
+        private byte registerHighVal = 0x42;
+        private byte registerLowVal = 0x65;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="address">Enumeration type of AddressSetting</param>
+        /// <param name="mode">Enumeration type of MeasurementMode</param>
+        public BH1750FVI(AddressSetting address, MeasurementMode mode)
+        {
+            sensorAddress = (byte)address;
+            sensorMode = (byte)mode;
+
+            if (mode == MeasurementMode.ContinuouslyHighResolutionMode2 || mode == MeasurementMode.OneTimeHighResolutionMode2)
+            {
+                sensorResolution = 2;
+            }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="address">Enumeration type of AddressSetting</param>
+        /// <param name="mode">Enumeration type of MeasurementMode</param>
+        /// <param name="transmittance">Enumeration type of LightTransmittance</param>
+        public BH1750FVI(AddressSetting address, MeasurementMode mode, LightTransmittance transmittance)
+        {
+            sensorAddress = (byte)address;
+            sensorMode = (byte)mode;
+
+            if (mode == MeasurementMode.ContinuouslyHighResolutionMode2 || mode == MeasurementMode.OneTimeHighResolutionMode2)
+            {
+                sensorResolution = 2;
+            }
+
+            switch (transmittance)
+            {
+                case LightTransmittance.Fifty:
+                    {
+                        registerHighVal = 0x44;
+                        registerLowVal = 0x6A;
+                        sensorTransmittance = 0.5;
+                    }
+                    break;
+                case LightTransmittance.Eighty:
+                    {
+                        registerHighVal = 0x42;
+                        registerLowVal = 0x76;
+                        sensorTransmittance = 0.8;
+                    }
+                    break;
+                case LightTransmittance.Hundred:
+                    {
+                        registerHighVal = 0x42;
+                        registerLowVal = 0x65;
+                    }
+                    break;
+                case LightTransmittance.Hundred_Twenty:
+                    {
+                        registerHighVal = 0x41;
+                        registerLowVal = 0x7A;
+                        sensorTransmittance = 1.2;
+                    }
+                    break;
+                case LightTransmittance.Hundred_Fifty:
+                    {
+                        registerHighVal = 0x41;
+                        registerLowVal = 0x7E;
+                        sensorTransmittance = 1.5;
+                    }
+                    break;
+                case LightTransmittance.Two_Hundred:
+                    {
+                        registerHighVal = 0x41;
+                        registerLowVal = 0x73;
+                        sensorTransmittance = 2;
+                    }
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Initialize BH1750FVI
+        /// </summary>
+        public async Task InitializeAsync()
+        {
+            var settings = new I2cConnectionSettings(sensorAddress);
+            settings.BusSpeed = I2cBusSpeed.FastMode;                     
+
+            var controller = await I2cController.GetDefaultAsync();
+            sensor = controller.GetDevice(settings);
+
+            sensor.Write(new byte[] { 0x01 });
+            sensor.Write(new byte[] { registerHighVal });
+            sensor.Write(new byte[] { registerLowVal });
+        }
+
+        /// <summary>
+        /// Read data from BH1750FVI
+        /// </summary>
+        /// <returns>A double type contains data</returns>
+        public double Read()
+        {
+            byte[] readBuf = new byte[2];
+
+            sensor.WriteRead(new byte[] { sensorMode }, readBuf);
+
+            byte temp = readBuf[0];
+            readBuf[0] = readBuf[1];
+            readBuf[1] = temp;
+
+            double result = BitConverter.ToUInt16(readBuf, 0) / (1.2 * sensorResolution * sensorTransmittance);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Cleanup
+        /// </summary>
+        public void Dispose()
+        {
+            sensor.Dispose();
+        }
+    }
+}
---
+++ BH1750FVIDemo/BH1750FVIDemo/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+
+namespace BH1750FVIDemo
+{
+    public sealed partial class MainPage : Page
+    {
+        BH1750FVI sensor = new BH1750FVI(AddressSetting.AddPinLow, MeasurementMode.ContinuouslyHighResolutionMode, LightTransmittance.Hundred);
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Loaded += MainPage_Loaded;
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {           
+            await sensor.InitializeAsync();
+            
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            Data.Text = sensor.Read().ToString("0.00");
+        }
+    }
+}
