9daf98a65ffe229b18ccbf38012f0a8aa855abb6
ZhangGaoxing <993638176@qq.com>
2017-01-29 21:46:44 +0800

    Temperature & Humidity Sensor

---
+++ DHT11Demo/DHT11Demo/DHT11.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+
+namespace DHT11Demo
+{
+    /// <summary>
+    /// DHT11 data struct
+    /// </summary>
+    struct DHT11Data
+    {
+        /// <summary>
+        /// Temperature - ℃
+        /// </summary>
+        public short Temperature;
+        /// <summary>
+        /// Humidity - %
+        /// </summary>
+        public short Humidity;
+        /// <summary>
+        /// Check the data is true or false
+        /// </summary>
+        public bool IsTrue;
+    }
+
+    class DHT11
+    {
+        GpioPin sensor;
+        int pin;
+        Stopwatch sw = new Stopwatch();
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="gpioPin">DHT11 OUT Pin value</param>
+        public DHT11(int gpioPin)
+        {
+            pin = gpioPin;
+        }
+
+        /// <summary>
+        /// Initialize DHT11
+        /// </summary>
+        public void Initialize()
+        {
+            var gpio = GpioController.GetDefault();
+            sensor = gpio.OpenPin(pin);
+            sensor.SetDriveMode(GpioPinDriveMode.Output);
+            sensor.Write(GpioPinValue.High);
+
+            sw.Restart();
+            while (sw.Elapsed.TotalSeconds <= 1) ;
+            sw.Stop();
+        }
+
+        /// <summary>
+        /// Read data from DHT11
+        /// </summary>
+        /// <returns>DHT11 data struct</returns>
+        public DHT11Data Read()
+        {
+            string readString = null;
+            string[] readBuf = new string[5];
+            byte[] tempBuf = new byte[2];
+            byte[] humBuf = new byte[2];
+            byte check;
+            DHT11Data data;
+
+            sensor.Write(GpioPinValue.Low);
+
+            sw.Restart();
+            while (sw.Elapsed.TotalMilliseconds <= 18) ;
+            sw.Stop();
+
+            sensor.Write(GpioPinValue.High);
+
+            sw.Restart();
+            while (sw.Elapsed.TotalMilliseconds <= 0.03) ;
+            sw.Stop();
+
+            sensor.SetDriveMode(GpioPinDriveMode.Input);
+
+            sw.Restart();
+            while (sw.Elapsed.TotalMilliseconds <= 0.08) ;
+            sw.Stop();
+
+            sw.Restart();
+            while (sw.Elapsed.TotalMilliseconds <= 0.08) ;
+            sw.Stop();
+
+            for (int i = 0; i < 40; i++)
+            {
+                string result = "0";
+
+                while (sensor.Read() == GpioPinValue.Low) ;
+
+                if (sensor.Read() == GpioPinValue.High)
+                {
+                    sw.Restart();
+                    while (sw.Elapsed.TotalMilliseconds <= 0.028) ;
+                    sw.Stop();
+
+                    if (sensor.Read() == GpioPinValue.High)
+                    {
+                        result = "1";
+                        sw.Restart();
+                        while (sw.Elapsed.TotalMilliseconds <= 0.05) ;
+                        sw.Stop();
+                    }
+                }
+
+                readString += result;
+                if (i == 7 || i == 15 || i == 23 || i == 31)
+                {
+                    readString += ",";
+                }
+            }
+
+            readBuf = readString.Split(',');
+            tempBuf[0] = Convert.ToByte(readBuf[2], 2);
+            tempBuf[1] = Convert.ToByte(readBuf[3], 2);
+            humBuf[0] = Convert.ToByte(readBuf[0], 2);
+            humBuf[1] = Convert.ToByte(readBuf[1], 2);
+            check = Convert.ToByte(readBuf[4], 2);
+
+            data.Temperature = BitConverter.ToInt16(tempBuf, 0);
+            data.Humidity = BitConverter.ToInt16(humBuf, 0);
+
+            if (tempBuf[0] + tempBuf[1] + humBuf[0] + humBuf[1] == check)
+            {
+                data.IsTrue = true;
+            }
+            else
+            {
+                data.IsTrue = false;
+            }
+
+            sensor.SetDriveMode(GpioPinDriveMode.Output);
+            sensor.Write(GpioPinValue.High);
+
+            return data;
+        }
+
+        /// <summary>
+        /// Cleanup
+        /// </summary>
+        public void Dispose()
+        {
+            sensor.Dispose();
+        }
+    }
+}
---
+++ DHT11Demo/DHT11Demo/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace DHT11Demo
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        DHT11 sensor = new DHT11(3);
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            sensor.Initialize();
+            DispatcherTimer timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            DHT11Data data = sensor.Read();
+
+            if (data.IsTrue == true)
+            {
+                Temperature.Text = data.Temperature.ToString();
+                Humidity.Text = data.Humidity.ToString();
+            }
+
+            IsTrue.Text = data.IsTrue.ToString();
+        }
+    }
+}
