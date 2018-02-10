a48eca95aa336204f1f01ac85c42a0ae9337574e
Zhang Yuexin <zhangyuexin121@live.cn>
2018-02-10 16:44:50 +0800

    AT24CXX_TEST

---
+++ AT24CXX/AT24CXX_Demo/AT24CXX_Demo/AT24CXX.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.I2c;
+
+namespace AT24CXX_Demo
+{
+    class AT24CXX : IDisposable
+    {
+        private I2cDevice sensor;
+
+        private readonly byte AT_ADDR;
+
+        public AT24CXX(byte address)
+        {
+            AT_ADDR = address;
+        }
+
+        public async Task InitializeAsync()
+        {
+            var settings = new I2cConnectionSettings(AT_ADDR);
+            settings.BusSpeed = I2cBusSpeed.FastMode;
+
+            var controller = await I2cController.GetDefaultAsync();
+            sensor = controller.GetDevice(settings);
+        }
+
+        public async Task Write(byte start, byte[] buffer)
+        {
+            foreach (var item in buffer)
+            {
+                int index = Array.IndexOf(buffer, item);
+                sensor.Write(new byte[] { (byte)((start + index) >> 8), (byte)((start + index) & 0xFF), item });
+                
+                await Task.Delay(10);
+            }
+            
+        }
+
+        public byte[] Read(byte start, int length)
+        {
+            byte[] buffer = new byte[length];
+            byte[] temp = new byte[1];
+
+            for (int i = 0; i < length; i++)
+            {
+                sensor.WriteRead(new byte[] { (byte)((start + i) >> 8), (byte)((start + i) & 0xFF) }, temp);
+
+                buffer[i] = temp[0];
+            }
+
+            return buffer;
+        }
+
+        public void Dispose()
+        {
+            sensor.Dispose();
+        }
+    }
+}
---
+++ AT24CXX/AT24CXX_Demo/AT24CXX_Demo/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace AT24CXX_Demo
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            AT24CXX at = new AT24CXX(0x50);
+            await at.InitializeAsync();
+            //await at.Write(0, new byte[] { 1, 2, 3, 4, 5 });
+
+            var buffer = at.Read(50, 5);
+            foreach (var item in buffer)
+            {
+                Debug.WriteLine(item);
+            }
+        }
+    }
+}
